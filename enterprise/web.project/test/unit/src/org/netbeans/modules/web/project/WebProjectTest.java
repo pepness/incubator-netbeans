jGauravGupta/netbeans

Jakarta EE 10 support
Signed-off-by: Gaurav Gupta <gaurav.gupta@payara.fish>

enterprise/web.project/test/unit/src/org/netbeans/modules/web/project/WebProjectTest.java
@@ -111,6 +111,12 @@ public class WebProjectTest extends NbTestCase {
         JavaEEProjectSettings.setProfile(webProject, Profile.JAKARTA_EE_9_WEB);
         Profile obtainedProfileJakartaEE9 = JavaEEProjectSettings.getProfile(webProject);
         assertEquals(Profile.JAKARTA_EE_9_WEB, obtainedProfileJakartaEE9);
+        JavaEEProjectSettings.setProfile(webProject, Profile.JAKARTA_EE_91_WEB);
+        Profile obtainedProfileJakartaEE91 = JavaEEProjectSettings.getProfile(webProject);
+        assertEquals(Profile.JAKARTA_EE_91_WEB, obtainedProfileJakartaEE91);
+        JavaEEProjectSettings.setProfile(webProject, Profile.JAKARTA_EE_10_WEB);
+        Profile obtainedProfileJakartaEE10 = JavaEEProjectSettings.getProfile(webProject);
+        assertEquals(Profile.JAKARTA_EE_10_WEB, obtainedProfileJakartaEE10);
     }
 
     /**

pepness/incubator-netbeans

enterprise/web.project/test/unit/src/org/netbeans/modules/web/project/WebProjectTest.java
@@ -111,6 +111,12 @@ public class WebProjectTest extends NbTestCase {
         JavaEEProjectSettings.setProfile(webProject, Profile.JAKARTA_EE_9_WEB);
         Profile obtainedProfileJakartaEE9 = JavaEEProjectSettings.getProfile(webProject);
         assertEquals(Profile.JAKARTA_EE_9_WEB, obtainedProfileJakartaEE9);
+        JavaEEProjectSettings.setProfile(webProject, Profile.JAKARTA_EE_9_1_WEB);
+        Profile obtainedProfileJakartaEE91 = JavaEEProjectSettings.getProfile(webProject);
+        assertEquals(Profile.JAKARTA_EE_9_1_WEB, obtainedProfileJakartaEE91);
+        JavaEEProjectSettings.setProfile(webProject, Profile.JAKARTA_EE_10_WEB);
+        Profile obtainedProfileJakartaEE10 = JavaEEProjectSettings.getProfile(webProject);
+        assertEquals(Profile.JAKARTA_EE_10_WEB, obtainedProfileJakartaEE10);
     }
 
     /**